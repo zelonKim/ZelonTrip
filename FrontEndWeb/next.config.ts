bfc4f836ee6ae1ec5ee9485f25546f4b28acb71e
zelonKim <ksz18601@gmail.com>
2026-09-11 14:29:47 +0900

    deploy2

--- FrontEndWeb/next.config.ts
+++ FrontEndWeb/next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  outputFileTracingIncludes: {
+    "api/**/*": ["./node_modules/**/*"],
+  },
 };
 
 export default nextConfig;
